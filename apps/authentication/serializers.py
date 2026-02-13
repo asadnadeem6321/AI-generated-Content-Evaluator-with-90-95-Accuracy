9c08f8a34ab7f82f2923c508ab7e98a8f1f05b5f
Muhammad Asad Nadeem <masadnadeem23@gmail.com>
2026-02-13 15:29:56 +0500

    authentication/view is created and registe and profile end point is created

--- apps/authentication/serializers.py
+++ apps/authentication/serializers.py
@@ -11,22 +11,21 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'created_at')
 
     
-    class RegistrationSerializer(serializers.ModelSerializer):
-        '''Serializer for user registration'''
-
-        password = serializers.CharField(write_only=True, validators=[validate_password])
-        confirm_password = serializers.CharField(write_only=True)
-
-        class Meta:
-            model = User
-            fields = ['email', 'username', 'first_name', 'last_name', 'password', 'confirm_password', 'role', ]
-
-        def validate(self, attrs):
-            if attrs['password'] != attrs['confirm_password']:
-                raise serializers.ValidationError({"Password : Password and confirm password did not match"})
-            return attrs
-        
-        def create(self, validated_data):
-            validated_data.pop('confirm_password')
-            user = User.objects.create(**validated_data)
-            return user
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration"""
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'confirm_password', 'first_name', 'last_name', 'role']
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Passwords don't match"})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        return user
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# Models import 
+from .models import User
+
+# Serializers import
+from .serializers import RegisterSerializer, UserSerializer
 
 # Create your views here.
+
+class AuthViewSet(viewsets.GenericViewSet):
+
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'register':
+            return RegisterSerializer
+        return UserSerializer
+    
+    def get_permissions(self):
+        if self.action == ['register', 'login']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
+    
+    @action(detail=False, methods=['post'])
+    def register(self, request):
+        '''
+        User Registration
+        
+        request: 
+            email, 'email', 'username', 'password', 'confirm_password', 'first_name', 'last_name', 'role'
+
+        response:
+            user, token{refresh, access}
+        '''
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Creating token for this user
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'user':UserSerializer(user).data,
+            'tokens':{
+                'refresh':str(refresh),
+                'access':str(refresh.access_token),
+            }
+            }, status=status.HTTP_200_OK)
+    
+
+    @action(detail=False, methods=['get', 'put', 'patch'])
+    def profile(self, request):
+        '''
+        Get or update user profile
+
+        used in dashboard to show user profile and update it
+        '''
+        if request.method == 'get':
+            serializer = UserSerializer(request.user)
+            return Response(serializer.data)
+        
+        serializer = UserSerializer(request.user, data = request.data, partial=True )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
