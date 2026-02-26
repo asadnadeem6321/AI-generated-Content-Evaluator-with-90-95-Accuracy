b6cdac0201e8984cee9d2bb02b782798e52ed043
Muhammad Asad Nadeem <masadnadeem23@gmail.com>
2026-02-26 22:03:58 +0500

    working on results/model.py

--- apps/results/models.py
+++ apps/results/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from submissions.models import Submission
 # Create your models here.
+
+
+class Result(models.Model):
+    """Stores overall analysis result of a submission"""
+    submission = models.OneToOneField(Submission, on_delete=models.CASCADE, related_name='result')
+
+    ai_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.00, help_text='AI content percentage')
