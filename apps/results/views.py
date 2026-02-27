7d6a849a95066edaed7cfa8feffa633d077915fa
Muhammad Asad Nadeem <masadnadeem23@gmail.com>
2026-02-27 20:53:23 +0500

    results/views.py done for yet

--- apps/results/views.py
+++ apps/results/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.http import FileResponse
+from apps.authentication.permissions import IsTeacher
+
+# Model import
+from .models import Result, ParagraphResult
+# Serializer import
+from .serializers import ResultSerializer, ResultSummarySerializer, ParagraphResultSerializer
 
 # Create your views here.
+
+
+class ResultViewSet(viewsets.ReadOnlyModelViewSet):
+    """View set for viewing analysis results"""
+
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ResultSummarySerializer
+        return ResultSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_teacher():
+            # Teacher can see results from there classes
+            return Result.objects.filter(submission__class_obj__teacher = user)
+        else:
+            # Students and guest can only see their own results
+            return Result.objects.filter(submission__user=user)
+
+
+    @action(detail=True, methods=['get'])
+    def report(self, request, pk=None):
+        """Download pdf report"""
+        result = self.get_object()
+        
+        if not result.report_pdf:
+            return Response(
+                {'error': 'Report not generated yet'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        return FileResponse(
+            result.report_pdf.open('rb'),
+            as_attachment=True,
+            filename=f'report_{result.submission.assignment_name}.pdf'
+        )
+    
+    @action(detail=True, methods=['get'])
+    def paragraphs(self, request, pk=None):
+        """Get detailed paragraph-level results"""
+        result = self.get_object()
+        paragraphs = result.paragraphs.all()
+        serializer = ParagraphResultSerializer(paragraphs, many=True)
+        return Response(serializer.data)
